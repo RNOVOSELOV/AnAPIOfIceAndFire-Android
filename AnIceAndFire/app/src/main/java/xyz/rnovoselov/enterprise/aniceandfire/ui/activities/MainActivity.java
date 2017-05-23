f9aa0bcbe663748f7c2db4afc073069c760d8c86
Роман Новоселов <roman@MacBook-Pro-Roman.local>
2017-05-24 00:38:16 +0300

    MainActivity is added

--- AnIceAndFire/app/src/main/java/xyz/rnovoselov/enterprise/aniceandfire/ui/activities/MainActivity.java
+++ AnIceAndFire/app/src/main/java/xyz/rnovoselov/enterprise/aniceandfire/ui/activities/MainActivity.java
@@ -1,14 +1,37 @@
 package xyz.rnovoselov.enterprise.aniceandfire.ui.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import xyz.rnovoselov.enterprise.aniceandfire.R;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.fab)
+    FloatingActionButton floatingActionButton;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
